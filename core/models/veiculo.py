5409d808b49cd59c2cedfe21c1b446aeffc1f4df
manuelaluchtenbergsilva.ifc@gmail.com <manuelaluchtenbergsilva.ifc@gmail.com>
2025-04-22 14:56:24 -0300

    Fix Veiculo.acessorio

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,11 +4,13 @@ from .modelo import Modelo
 from .cor import Cor
 from .acessorio import Acessorio
 
+
 class Veiculo(models.Model):
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, null=True)
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, null=True)
-    acessorios = models.ManyToManyField(Acessorio, on_delete=models.PROTECT, null=True)
+    acessorios = models.ManyToManyField(Acessorio, null=True)
+
     def __str__(self):
         return f"({self.id}) {self.modelo}, {self.cor}, {self.ano} "
