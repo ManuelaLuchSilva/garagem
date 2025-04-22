040498a2ab175e4d63dc785fe47a933e9c2be1cf
manuelaluchtenbergsilva.ifc@gmail.com <manuelaluchtenbergsilva.ifc@gmail.com>
2025-04-22 14:15:56 -0300

    feat: criação da API para Veiculo

---
+++ core/models/veiculo.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from .modelo import Modelo
+from .cor import Cor
+from .acessorio import Acessorio
+
+class Veiculo(models.Model):
+    ano = models.IntegerField(null=True, default=0)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, null=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, null=True)
+    acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, null=True)
+    def __str__(self):
+        return f"({self.id}) {self.modelo}, {self.cor}, {self.ano} "
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,4 +1,5 @@
 from .user import UserViewSet
 from .acessorio import AcessorioViewSet
 from .cor import CorViewSet
-from .modelo import ModelViewSet
+from .modelo import ModeloViewSet
+from .veiculo import VeiculoViewSet
